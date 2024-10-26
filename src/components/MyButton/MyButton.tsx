f7dc2a79f214ff669dc113a91340b279c81e42b5
“GiselaRodriguezMayor04” <giselarodriguezmayor2004@gmail.com>
2024-10-26 22:56:43 +0100

    GiselaRodMay

--- src/components/MyButton/MyButton.tsx
+++ src/components/MyButton/MyButton.tsx
@@ -1,8 +1,6 @@
-// src/components/mybutton/mybutton.tsx
 import { MouseEventHandler } from 'react';
 import Button from '@mui/material/Button';
 
-// Declaramos la interface para las props del componente
 export interface MyButtonProps {
   text: string; 
   txtcolor: string; 
@@ -16,9 +14,7 @@ export interface MyButtonProps {
   onClick?: MouseEventHandler<HTMLButtonElement>; 
 }
 
-// Definimos el componente MyButton
 function MyButton(props: MyButtonProps) {
-  // Extraemos las props
   const {
     text,
     txtcolor,
@@ -39,7 +35,6 @@ function MyButton(props: MyButtonProps) {
       disabled={disabled}
       size={size}
       variant="outlined"
-      // Estilos personalizados con la propiedad sx de MUI
       sx={{
         color: txtcolor,
         backgroundColor: bgcolor,
